7d9698433992ae71def136406437f56e13d3b172
nico-mania <55563178+nico-mania@users.noreply.github.com>
2021-06-24 12:40:26 +0200

    added CameraZoom script and orbiting cam
    didn't test on android
    
    player seems bugged

---
+++ Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Source: https://pressstart.vip/tutorials/2018/07/12/44/pan--zoom.html
+public class CameraZoom : MonoBehaviour
+{
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 9;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.01f);
+        }
+            zoom(Input.GetAxis("Mouse ScrollWheel"));
+    }
+
+    void zoom(float increment)
+    {
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment*10, zoomOutMin, zoomOutMax);
+    }
+}
