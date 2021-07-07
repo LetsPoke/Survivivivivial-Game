42bfb24608064ba9e2a98a07474b2bc471cbbfd1
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2021-07-07 17:42:16 +0200

    AR stuff

--- Assets/Scripts/AR Scripts/SceneChanger.cs
+++ Assets/Scripts/AR Scripts/SceneChanger.cs
@@ -11,10 +11,10 @@ public class SceneChanger : MonoBehaviour
 
         if(SceneManager.GetActiveScene().name == "Spielwelt"){
             SceneManager.LoadScene("AR_Scene"); 
-            Debug.Log("go ar");
+            //Debug.Log("go ar");
         }else if(SceneManager.GetActiveScene().name == "AR_Scene"){
             SceneManager.LoadScene("Spielwelt");
-            Debug.Log("go 1");
+            //Debug.Log("go 1");
         }
     }
 }
